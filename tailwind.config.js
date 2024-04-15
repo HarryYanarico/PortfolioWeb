2f55c12bc499504fb150701d373c93f69c0f4575
Harry <harryyanarico19@gmail.com>
2024-04-15 10:45:32 -0400

    mas cambios

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+tailwind.config = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        'slide': {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(-100%)' },
+        },
+        'trans-right': {
+          '0%, 100%': { transform: 'translateX(10px)' },
+          '50%': { transform: 'translateX(0)' }
+        }
+      },
+      animation: {
+        'slide': 'slide 20s infinite linear',
+        'trans-right': 'trans-right 1.5s ease-in-out infinite'
+      },
+      colors: {
+        clifford: '#da373d',
+      }
+    },
+  },
+  plugins: [],
+}
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
