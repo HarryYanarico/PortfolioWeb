acd2854b53c8e549a00de3368a4d7c103fc61d26
Harry <harryyanarico19@gmail.com>
2025-03-24 16:02:56 -0400

    ajustes responsives

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+//   tailwind.config = {
+//     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//     theme: {
+//       extend: {
+//         backgroundImage: {
+//           andapp: "url(images/AndroidDev.png)",
+//           DevWeb:
+//             "url('images/appweb.png')",
+//           software:
+//             "url('images/software.jpg')",
+//           game: "url('images/game.jpg')",
+//           fondo:
+//             "url('https://png.pngtree.com/background/20210711/original/pngtree-blue-technology-future-artificial-intelligence-science-picture-image_1125424.jpg')",
+//           perfil:
+//             "url(images/perfil/perfil02.avif)",
+//         },
+//         keyframes: {
+//           slide: {
+//             "0%": { transform: "translateY(0)" },
+//             "100%": { transform: "translateY(-100%)" },
+//           },
+//           "trans-right": {
+//             "0%, 100%": { transform: "translateX(10px)" },
+//             "50%": { transform: "translateX(0)" },
+//           },
+//         },
+//         animation: {
+//           slide: "slide 20s infinite linear",
+//           "trans-right": "trans-right 1.5s ease-in-out infinite",
+//         },
+//         colors: {
+//           clifford: "#da373d",
+//         },
+//       },
+//     },
+//     plugins: [],
+//   };
